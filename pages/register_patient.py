e6fe0ca09d4c922f6e8be418904d0039f1899091
dushan6693 <dushanmadushankabeligala9@gmail.com>
2025-05-20 21:57:45 +0530

    fix: Gender option with Male Female

--- pages/register_patient.py
+++ pages/register_patient.py
@@ -6,7 +6,13 @@ def render():
 
     with st.form("register_form"):
         name = st.text_input("Full Name")
-        gender = st.selectbox("Gender", ["M", "F"])
+        gender = st.selectbox("Gender", ["Male", "Female"]) # display Male and Female for give good user experience
+        # this send M and F to the database
+        if gender == "Male":
+            gender = "M"
+        else:
+            gender = "F"
+
         dob = st.date_input("Date of Birth")
         dob_str = dob.strftime('%Y-%m-%d')  
 
